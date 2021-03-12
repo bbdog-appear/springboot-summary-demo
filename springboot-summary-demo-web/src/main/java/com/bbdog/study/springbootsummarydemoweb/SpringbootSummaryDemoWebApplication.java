f83ac3939d837bf8c053ff87bf8fc1aaea06c4ef
bbdog-appear <1539113649@qq.com>
2021-03-12 17:05:42 +0800

    wangcheng - springboot学习项目初版 - 2021年3月12日17:05:37

---
+++ springboot-summary-demo-web/src/main/java/com/bbdog/study/springbootsummarydemoweb/SpringbootSummaryDemoWebApplication.java
@@ -0,0 +1,13 @@
+package com.bbdog.study.springbootsummarydemoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSummaryDemoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSummaryDemoWebApplication.class, args);
+    }
+
+}
