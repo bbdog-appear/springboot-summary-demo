a78eb67c524df9fbd1ccbe1b3ff58f3561231d1b
cheng.wang <cheng.wang@huifu.com>
2022-07-18 23:52:37 +0800

    黑马学习视频 - 通信相关-TCP通信，改为服务端可以接收多个客户端请求，即每接收一个客户端socket通信管道的连接请求，就开启一个线程。其中注意，如果某个客户端关闭了，那么其和服务端的socket通信管道也就随之断开，即服务端这边为该客户端开启的线程中的socket管道断开，那么就会抓到异常，最后一定要注意关闭资源，原因也写在注释里了，涉及到垃圾回收相关 - 2022年7月18日23:52:14

--- springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/javabase/heimastudy/通信相关/TCP通信/TestTCPServer.java
+++ springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/javabase/heimastudy/通信相关/TCP通信/TestTCPServer.java
@@ -1,9 +1,6 @@
 package com.bbdog.study.springboot.summary.demo.web.javabase.heimastudy.通信相关.TCP通信;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -21,6 +18,7 @@ import java.net.Socket;
  *      因为还在等待第二行消息，而这时候因为客户端死了，所以服务端也跟着死，再改进的话，可以把while改为if即可，即服务端接收一条后也死，就不会报错。
  *      4、改为循环发消息的方式，但是这种情况下，服务端只能接收一个客户端请求，即再启动一个客户端，发消息时，服务端接收不到消息的，原因是服务端的
  *      main方法Socket socket = ss.accept();在第一次客户端连接后就以后往后面执行了，第二个客户端建立socket连接时，这里1、2代码都不会跑到的
+ *      5、再改为服务端可以接收多个客户端请求，即每来一个客户端请求，服务端这边开启一个线程执行，并发执行互不影响。
  * </p>
  *
  * @author cheng.wang
@@ -33,26 +31,60 @@ public class TestTCPServer {
         // 1、注册端口
         ServerSocket ss = new ServerSocket(8888);
 
-        // 2、开始等待接收客户端的socket管道连接，意思是客户端那边的socket要和服务端这边碰面，然后服务端接受连接，
-        // 如果客户端那边还没有请求该服务端的ip和端口，这边会一直等待
-        Socket socket = ss.accept();
+        while (true) {
+            // 2、开始等待接收客户端的socket管道连接，意思是客户端那边的socket要和服务端这边碰面，然后服务端接受连接，
+            // 如果客户端那边还没有请求该服务端的ip和端口，这边会一直等待，即无socket管道请求时，这里会阻塞
+            Socket socket = ss.accept();
 
-        // 3、从socket通信管道中得到一个字节输入流，这个输入流就是通向socket通信管道的，所以通过这个输入流读取的话，
-        // 可以读到当时客户端写到socket通信管道中的字节数据
-        InputStream is = socket.getInputStream();
-
-        // 4、把字节输入流转换成字符输入流
-        Reader isr = new InputStreamReader(is);
-
-        // 5、把字符输入流包装成缓冲字符输入流
-        BufferedReader br = new BufferedReader(isr);
-
-        // 6、按照行读取消息，这里因为包装了很多层，但其实底层还是由低级的字节输入流FileInputStream去读取数据的，然后把数据送到高级输入流做加工，
-        // 无论是字符还是数字，在计算机底层都是二进制传输的，字符是数据，字节是大小单位，按照字节读，就相当于数那个二进制的位数，按照字符读，相当于直接一个一个字符读
-        String msg;
-        while ((msg = br.readLine()) != null) {
-            System.out.println("服务端收到：" + msg);
+            // 7、每接收一个客户端socket通信管道的连接请求，就开启一个线程执行
+            new ServerReadThread(socket).start();
         }
     }
 
+}
+
+class ServerReadThread extends Thread {
+
+    private final Socket socket;
+
+    public ServerReadThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // 3、从socket通信管道中得到一个字节输入流，这个输入流就是通向socket通信管道的，所以通过这个输入流读取的话，
+            // 可以读到当时客户端写到socket通信管道中的字节数据
+            InputStream is = socket.getInputStream();
+
+            // 4、把字节输入流转换成字符输入流
+            Reader isr = new InputStreamReader(is);
+
+            // 5、把字符输入流包装成缓冲字符输入流
+            BufferedReader br = new BufferedReader(isr);
+
+            // 6、按照行读取消息，这里因为包装了很多层，但其实底层还是由低级的字节输入流FileInputStream去读取数据的，然后把数据送到高级输入流做加工，
+            // 无论是字符还是数字，在计算机底层都是二进制传输的，字符是数据，字节是大小单位，按照字节读，就相当于数那个二进制的位数，按照字符读，相当于直接一个一个字符读
+            String msg;
+            while ((msg = br.readLine()) != null) {
+                System.out.println("服务端收到客户端：" + socket.getRemoteSocketAddress() + "的消息：" + msg);
+            }
+        } catch (Exception e) {
+            /* 这里一般是某个客户端下线了，然后这边的socket管道肯定就断了，即抓到客户端下线异常。那抓到异常后，这个线程(客户端请求)肯定跑结束了
+            * 但是一定要注意要把socket资源关闭，如果这里不关闭，可以看到虽然这个线程跑完了，即线程对象ServerReadThread被销毁了，其中该对象的
+            * 实例变量socket肯定也会被垃圾回收掉，但是原来线程外面的socket对象，其实还有引用指向的(因为外层的方法没跑完，是个死循环)，那么这样
+            * 就造成原来客户端的socket对象请求一直不会被回收，久而久之socket对象越来越多，造成内存泄漏，会导致系统oom。因此这里线程跑完后应该
+            * 关闭socket资源 */
+            System.out.println("客户端：" + socket.getRemoteSocketAddress() + "下线了");
+        } finally {
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
