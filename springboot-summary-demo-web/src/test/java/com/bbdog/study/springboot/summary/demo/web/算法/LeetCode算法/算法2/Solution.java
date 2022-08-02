42cd3eba360cbf31746392ccf6525906abf0908f
cheng.wang <cheng.wang@huifu.com>
2022-08-02 20:18:11 +0800

    java算法-算法练习2-判断一个数字是否是回文数。 - 2022年8月2日20:18:04

---
+++ springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/算法/LeetCode算法/算法2/Solution.java
@@ -0,0 +1,35 @@
+package com.bbdog.study.springboot.summary.demo.web.算法.LeetCode算法.算法2;
+
+/**
+ * <p>
+ *      1221
+ * </p>
+ *
+ * @author cheng.wang
+ * @version Id：Solution.java Date：2022/8/2 19:23 Version：1.0
+ */
+public class Solution {
+
+    public boolean isPalindrome(int x) {
+        // 负数肯定回文数不一样
+        if (x < 0) {
+            return false;
+        }
+        // 个位为0的，不可能是回文数，因为首位不可能是0，即没有012210这样的数
+        if (x % 10 == 0) {
+            return false;
+        }
+        int right = 0;
+        while (x > right) {
+            right = right * 10 + x % 10;
+            x = x / 10;
+        }
+        if (x < right) {
+            if (x == right / 10) {
+                return true;
+            }
+        }
+        return x == right;
+    }
+
+}
---
+++ springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/算法/LeetCode算法/算法2/测试回文数.java
@@ -0,0 +1,26 @@
+package com.bbdog.study.springboot.summary.demo.web.算法.LeetCode算法.算法2;
+
+/**
+ * <p>
+ *      判断一个数是否是回文数：
+ *
+ *      给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ *
+ *      回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ *      例如，121 是回文，而 123 不是。
+ *
+ *      进阶：你能不将整数转为字符串来解决这个问题吗？
+ *
+ * </p>
+ *
+ * @author cheng.wang
+ * @version Id：测试回文数.java Date：2022/8/2 19:21 Version：1.0
+ */
+public class 测试回文数 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
