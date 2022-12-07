3c3922859e4f1b1e6846db9105cccb57b0001e77
cheng.wang <cheng.wang@huifu.com>
2022-12-07 19:53:40 +0800

    java算法-算法练习-两个正序数组的中位数，正确版，但是时间复杂度是O(m + n)，并不符合题目要求的时间复杂度O(log(m+n))，后面可以继续优化，使用二分查找法。 - 2022年12月7日19:53:06

--- springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/算法/LeetCode算法/寻找两个正序数组的中位数/Solution.java
+++ springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/算法/LeetCode算法/寻找两个正序数组的中位数/Solution.java
@@ -31,8 +31,8 @@ package com.bbdog.study.springboot.summary.demo.web.算法.LeetCode算法.寻找
 public class Solution {
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 2};
-        int[] nums2 = {3, 4};
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2, 7};
         double medianSortedArrays = new Solution().findMedianSortedArrays(nums1, nums2);
         System.out.println(medianSortedArrays);
     }
@@ -40,22 +40,32 @@ public class Solution {
     /**
      * 我的思路：找出两个数组中的最小值，再找出两个数组中的最大值，用(最小值+最大值)/2
      *
+     * 上面的理解就是错的，中位数的定义：如果数据的个数是奇数，则中间那个数据就是这群数据的中位数，
+     * 如果数据的个数是偶数，则中间那2个数据的算术平均值就是这群数据的中位数
+     * 1、先合并两个数组并排序-使用归并排序
+     * 2、如果是奇数，则取中间的那个数，如果是偶数，则两个数的平均数
+     *
      * @param nums1 数组1
      * @param nums2 数组2
      * @return 中位数
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length != 0 && nums2.length != 0) {
-            int min = Math.min(nums1[0], nums2[0]);
-            int max = Math.max(nums1[nums1.length - 1], nums2[nums2.length - 1]);
-            return (min + max) / 2.0;
-        } else if (nums2.length != 0) {
-            return (nums2[0] + nums2[nums2.length - 1]) / 2.0;
-        } else if (nums1.length != 0) {
-            return (nums1[0] + nums1[nums1.length - 1]) / 2.0;
-        } else {
-            return 0.0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        // 两个数组合并后并排序的数据
+        int[] numSum = new int[nums1.length + nums2.length];
+        while (i < nums1.length || j < nums2.length) {
+            // 当其中一个数组遍历完成后，将另一个数组剩下的所有元素复制到合并数组列尾
+            if (j == nums2.length || (i != nums1.length && nums1[i] < nums2[j])) {
+                numSum[k++] = nums1[i++];
+            } else {
+                numSum[k++] = nums2[j++];
+            }
         }
+        // 如果是奇数则直接取中间那个数，如果是偶数则取中间两位数的平均值
+        return numSum.length % 2 != 0 ? (double)numSum[numSum.length / 2] :
+                ((double)numSum[(numSum.length / 2) - 1] + (double)numSum[numSum.length / 2]) / 2;
     }
 
 }
