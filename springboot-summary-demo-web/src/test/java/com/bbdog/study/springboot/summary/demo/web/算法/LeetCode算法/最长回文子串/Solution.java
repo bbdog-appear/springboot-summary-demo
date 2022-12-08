8361b7818d36619656412b956d08bb8d4fae14fa
cheng.wang <cheng.wang@huifu.com>
2022-12-08 14:53:58 +0800

    java算法-算法练习-找到s中最长的回文子串，自己写的这个算法是错的，不符合要求，且空间复杂度很高。 - 2022年12月8日14:53:53

---
+++ springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/算法/LeetCode算法/最长回文子串/Solution.java
@@ -0,0 +1,66 @@
+package com.bbdog.study.springboot.summary.demo.web.算法.LeetCode算法.最长回文子串;
+
+/**
+ * <p>
+ *      给你一个字符串s，找到s中最长的回文子串。
+ *
+ *      示例1：
+ *      输入：s="babad"
+ *      输出："bab"
+ *      解释："aba"同样是符合题意的答案。
+ *
+ *      示例2：
+ *      输入：s="cbbd"
+ *      输出："bb"
+ *
+ *      提示：
+ *      1<=s.length<=1000
+ *      s仅由数字和英文字母组成
+ * </p>
+ *
+ * @author cheng.wang
+ * @version Id：Solution.java Date：2022/12/8 10:43 Version：1.0
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        String s = new Solution().longestPalindrome("babad");
+        System.out.println(s);
+    }
+
+    /**
+     * 自己思路：
+     * 1、将字符串反转，记为reverse
+     * 2、使用两个指针遍历两个字符串，移动指针，将相同字符拼接成字符串，直到字符不相同时停止遍历，
+     * 该字符串就是回文数
+     *
+     * @param s 字符串
+     * @return 回文数子串
+     */
+    public String longestPalindrome(String s) {
+        // 字符长度为1时，该字符串就是回文字符。
+        if (s.length() == 1) {
+            return s;
+        }
+        char[] reverseChar = new char[s.length()];
+        int j = s.length()-1;
+        for (int i = 0; i < s.length(); i++) {
+            reverseChar[j--] = s.charAt(i);
+        }
+        int k = 0;
+        StringBuilder result = new StringBuilder();
+        boolean flag = false;
+        while (k < s.length()) {
+            if (s.charAt(k) == reverseChar[k]) {
+                result.append(s.charAt(k));
+                flag = true;
+            }
+            if (flag && s.charAt(k) != reverseChar[k]) {
+                break;
+            }
+            k++;
+        }
+        return result.toString();
+    }
+
+}
