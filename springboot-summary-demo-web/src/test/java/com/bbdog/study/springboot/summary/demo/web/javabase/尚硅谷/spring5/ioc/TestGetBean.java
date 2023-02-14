46783ce56bc52815d09194538e78c39bb7b6631b
cheng.wang <cheng.wang@huifu.com>
2023-02-15 03:06:33 +0800

    spring的ioc容器(工厂)中获取bean对象的演示 - 2023年2月15日03:06:17

---
+++ springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/javabase/尚硅谷/spring5/ioc/TestGetBean.java
@@ -0,0 +1,15 @@
+package com.bbdog.study.springboot.summary.demo.web.javabase.尚硅谷.spring5.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestGetBean {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        // 这里是从工厂里获取对象，不是通过new的方式，其中肯定运用反射机制。看下源码
+        User user = context.getBean("user", User.class);
+
+    }
+
+}
---
+++ springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/javabase/尚硅谷/spring5/ioc/User.java
@@ -0,0 +1,4 @@
+package com.bbdog.study.springboot.summary.demo.web.javabase.尚硅谷.spring5.ioc;
+
+public class User {
+}
