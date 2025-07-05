222612d2c993a7de73172cd5d9a39754a1a63b83
cheng.wang <1539113649@qq.com>
2025-07-06 07:33:35 +0800

    测试

--- springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/mine/Test.java
+++ springboot-summary-demo-web/src/test/java/com/bbdog/study/springboot/summary/demo/web/mine/Test.java
@@ -1,4 +1,9 @@
 package com.bbdog.study.springboot.summary.demo.web.mine;
 
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
 }
